eee7315481b367250c87011185fd835eebef0f1e
lazy-dolphin <rishahb@protonmail.com>
2020-05-01 17:34:07 +0530

    First commit

---
+++ src/main/java/pl/czak/minimal/MainActivity.kt
@@ -0,0 +1,16 @@
+package pl.czak.minimal
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		val label = TextView(this)
+		label.setText("Hello world!")
+
+		setContentView(label)
+	}
+}
